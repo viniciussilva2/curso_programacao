65890ae15d7e525fcf0568127433030b71d4980f
Vinicius Silva <vinyoli.vs@gmail.com>
2025-08-02 00:11:31 -0300

    Primeiro commit
    Primeiro commit do projeto.

---
+++ curso/src/curso/Main.java
@@ -0,0 +1,32 @@
+package curso;
+
+import java.util.Locale; //Importar LOCALE.
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	String product1 = "Computer";
+	String product2 = "Office desk";
+	
+	int age = 30;
+	int code = 5290;
+	char gender = 'F';
+	
+	double price1 = 2100.0;
+	double price2 = 650.50;
+	double measure = 53.234567;
+	
+	System.out.println("Products:");
+	System.out.println("Computer, witch price is " + "$"+ price1);
+	System.out.println("Office desk, which price is " + "$" + price2);
+	System.out.println("Record: " + age + " code " + code + " and gender: " + gender);
+	System.out.println("Measue with eight decimal places: " + measure);
+    System.out.printf(new Locale("pt", "BR"), "Rouded (three decimal places): %.3f%n", measure);
+    System.out.printf(Locale.US, "US decimal point: %.3f%n", measure);
+    
+    System.out.println("Exercício finalizado!!");
+		
+	}
+
+}
---
+++ src/curso_programacao/Main.java
@@ -0,0 +1,12 @@
+package curso_programacao;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Meu primeiro programa em java");
+
+		
+	}
+
+}
