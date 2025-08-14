9a7492cfbe726eb1d087003ce44a7372b70c2d8a
Vinicius Silva <vinyoli.vs@gmail.com>
2025-08-14 20:24:07 -0300

    Encapsulamento

--- curso/src/curso/Main.java
+++ curso/src/curso/Main.java
@@ -8,22 +8,54 @@ import entities.Triangle;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        entities.Product product = new entities.Product();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.useLocale(Locale.US);
+	
+		 
+		 System.out.println("Enterproductdata: ");
+		 
+		 System.out.print("Name: ");
+		 String name= sc.nextLine();
+		 
+		 System.out.print("Price: ");
+		 double price= sc.nextDouble();
+		 
+		 System.out.print("Quantityin stock: ");
+		 int quantity= sc.nextInt();
+		 
+		 Product product = new Product(name, price, quantity);
+		 
+		 System.out.println();
+		 
+		 System.out.println("Productdata: "+ product);
+		 
+		 System.out.println();
+		 
+		 System.out.print("Enter the number of products to be added in stock: ");
+		 quantity= sc.nextInt();
+		 product.addProducts(quantity);
+		 
+		 System.out.println();
+		 
+		 System.out.println("Updateddata: "+ product);
+		 
+		 System.out.println();
+		 
+		 System.out.print("Enter the number of products to be removed from stock: ");
+		 quantity= sc.nextInt();
+		 
+		 product.removeProducts(quantity);
+		 
+		 System.out.println();
+		 
+		 System.out.println("Updateddata: "+ product);
 
-        System.out.println("Enter product data: ");
-        System.out.print("Name: ");
-        product.name = sc.next();
-        
-        System.out.print("Price: ");
-        product.price = sc.nextDouble();
-        
-        System.out.print("Quantity in stock: ");
-        product.quantity = sc.nextInt();
-                
-        sc.close();
-    }
+		 
+	
+		
+
+		sc.close();
+	}
 
 }
--- curso/src/entities/Product.java
+++ curso/src/entities/Product.java
@@ -2,24 +2,60 @@ package entities;
 
 public class Product {
 
-	public String name;
+	private String name;
 	public double price;
 	public int quantity;
 
-	public double TotalValueInStock() {
+	public Product() {
+
+	}
+
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double totalValueInStock() {
 
 		return price * quantity;
 
 	}
 
-	public void AddProducts(int quantity) {
+	public void addProducts(int quantity) {
 
-		this.quantity += quantity; //Recebe o parâmetro e mais ele mesmo.
+		this.quantity += quantity; // Recebe o parâmetro e mais ele mesmo.
 	}
 
-	public void RemoveProducts(int quantity) {
+	public void removeProducts(int quantity) {
 
-		this.quantity -= quantity; //Retira o parâmetro e mais ele mesmo.
+		this.quantity -= quantity; // Retira o parâmetro e mais ele mesmo.
+	}
+
+	// Sobrepondo a versão padrão do toString do object.
+	public String toString() {
+		return name + ", $" + String.format("%.2f", price) + ", " + quantity + " units, Total: $ "
+				+ String.format("%.2f", totalValueInStock());
+	}
+
+	public static String formatarCPF(String cpf) {
+		return cpf.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
+	}
+
+	public static String formatarCNPJ(String cnpj) {
+
+		cnpj = cnpj.replaceAll("\\D", "");
+
+		return cnpj.replaceAll("(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})", "$1.$2.$3/$4-$5");
 	}
 
 }
